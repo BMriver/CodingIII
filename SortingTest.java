c59f87147c8b472603262a314fa977baaf4f232e
BMriver <133412692+BMriver@users.noreply.github.com>
2023-10-06 06:36:04 -0500

    Add files via upload

--- SortingTest.java
+++ SortingTest.java
@@ -3,47 +3,93 @@ import java.util.Random;
 
 public class SortingTest {
 
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Insertion Sort
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
     public static void main(String[] args) {
-        
-        // Generating random array
+
         int size = 100000;
         Random random = new Random();
         int[] array = new int[size];
         for (int i = 0; i < size; i++) {
             array[i] = 1 + random.nextInt(1000000);
         }
-        
-        // Copying arrays
+
         int[] bubbleArray = new int[size];
         int[] selectionArray = new int[size];
+        int[] insertionArray = new int[size];
         for (int i = 0; i < size; i++) {
             bubbleArray[i] = array[i];
             selectionArray[i] = array[i];
+            insertionArray[i] = array[i];
         }
-        
-        // Bubble Sort
+
         long start, finish, timeElapsed;
+
+        // Bubble Sort
         start = System.currentTimeMillis();
         int n = bubbleArray.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if (bubbleArray[j] > bubbleArray[j+1]) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (bubbleArray[j] > bubbleArray[j + 1]) {
                     int temp = bubbleArray[j];
-                    bubbleArray[j] = bubbleArray[j+1];
-                    bubbleArray[j+1] = temp;
+                    bubbleArray[j] = bubbleArray[j + 1];
+                    bubbleArray[j + 1] = temp;
                 }
             }
         }
         finish = System.currentTimeMillis();
         timeElapsed = finish - start;
         System.out.println("Bubble Sort time: " + timeElapsed + "ms");
-        
+
         // Selection Sort
         start = System.currentTimeMillis();
         n = selectionArray.length;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             int min_idx = i;
-            for (int j = i+1; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 if (selectionArray[j] < selectionArray[min_idx]) {
                     min_idx = j;
                 }
@@ -55,5 +101,24 @@ public class SortingTest {
         finish = System.currentTimeMillis();
         timeElapsed = finish - start;
         System.out.println("Selection Sort time: " + timeElapsed + "ms");
+
+        // Insertion Sort
+        start = System.currentTimeMillis();
+        n = insertionArray.length;
+        for (int i = 1; i < n; i++) {
+            int key = insertionArray[i];
+            int j = i - 1;
+
+            while (j >= 0 && insertionArray[j] > key) {
+                insertionArray[j + 1] = insertionArray[j];
+                j--;
+            }
+            insertionArray[j + 1] = key;
+        }
+        finish = System.currentTimeMillis();
+        timeElapsed = finish - start;
+        System.out.println("Insertion Sort time: " + timeElapsed + "ms");
     }
 }
+
+
