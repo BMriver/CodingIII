47ebca423fe881d8325b3bc0ea960c11f4405b9d
BMriver <133412692+BMriver@users.noreply.github.com>
2023-11-26 20:18:24 -0600

    Delete Stack.java

--- Stack.java
+++
@@ -1,8 +0,0 @@
-package drive;
-public abstract class Stack extends LinkedList {
-    public abstract void push(String data);
-
-    public abstract Node pop();
-
-    public abstract Node peek();
-}
